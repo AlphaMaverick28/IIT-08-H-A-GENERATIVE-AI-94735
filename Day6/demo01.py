b7ec74de16e37b0b4628eede612326fe7aa8808a
Atharv Koli <atharvkoli47@gmail.com>
2025-12-28 23:44:02 +0530

    Delete Day6 directory

--- Day6/demo01.py
+++
@@ -1,8 +0,0 @@
-from langchain.chat_models import init_chat_model
-from langchain.agents import create_agent 
-
-#create model 
-llm=init_chat_model (
-    model="google/gemma-3-4b",
-    model_provider="openai",
-    base_url="http://127
